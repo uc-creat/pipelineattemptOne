bc4abb08b25ec15ab6fe42c28f16f100c3feb056
uc-creat <utkarshchauhanutc26@gmail.com>
2023-07-19 22:07:58 +0530

    changed

--- cal.py
+++ cal.py
@@ -2,7 +2,7 @@ def cal(numberOne, numberTwo, operator):
   if operator=="sum":
     return numberOne+numberTwo
   elif operator=="sub":
-    return numberOne-numberTwo
+    return numberOne+numberTwo
   
 
       
