531e03419be850d4dfc250bfaa58191d53a21c95
uc-creat <utkarshchauhanutc26@gmail.com>
2023-07-19 20:23:27 +0530

    passing test cases

--- test_cal.py
+++ test_cal.py
@@ -6,9 +6,9 @@ class CalculatorTest(unittest.TestCase):
   def test_nothing(self):
     self.s = cal.cal(2,1,"sum")
     self.assertEqual(self.s,3)
-  def test_nothing(self):
-    self.s = cal.cal(2,1,"sum")
-    self.assertEqual(self.s,2)
+  # def test_nothing(self):
+  #   self.s = cal.cal(2,1,"sum")
+  #   self.assertEqual(self.s,2)
   
 
 # if __name__ == '__main__':
