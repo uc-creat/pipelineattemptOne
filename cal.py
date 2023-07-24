d282c783aeb1f72f365ce599591f29653f12375f
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2023-07-24 14:03:40 +0530

    commited

--- cal.py
+++ cal.py
@@ -2,7 +2,12 @@ def cal(numberOne, numberTwo, operator):
   if operator=="sum":
     return numberOne-numberTwo
   elif operator=="sub":
-    return numberOne+numberTwo
+    return numberOne+numberTwo + 1
   
+
+  
+
+
+
 
       
